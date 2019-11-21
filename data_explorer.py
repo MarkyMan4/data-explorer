399ec98667e9946d2492d3ad96a52f66fa26162b
Markus <markus.hutnik@gmail.com>
2019-11-20 22:43:59 -0600

    perform isometric regression on input data

--- data_explorer.py
+++ data_explorer.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from pandas.plotting import scatter_matrix
 import altair as alt
+from iso_regression import IsoRegressor
 import os
 
 @st.cache
@@ -27,40 +28,49 @@ c_boxes = []
 for i in range(len(data.columns)):
     c_boxes.append(st.sidebar.checkbox(data.columns[i]))
 
-st.sidebar.markdown('# ')
-st.sidebar.markdown('### See the data:')
-if st.sidebar.checkbox('Explore'):
+# only use columns selected in check boxes
+selected_cols = []
+for i in range(len(c_boxes)):
+    if c_boxes[i]:
+        selected_cols.append(data.columns[i])
 
-    # only use columns selected in check boxes
-    selected_cols = []
-    for i in range(len(c_boxes)):
-        if c_boxes[i]:
-            selected_cols.append(data.columns[i])
+display_data = data[selected_cols]
 
-    display_data = data[selected_cols]
+if len(selected_cols) > 0:
+    if st.checkbox('Show raw data'):
+        st.write(display_data)
 
-    if len(selected_cols) > 0:
-        if st.checkbox('Show raw data'):
-            st.write(display_data)
+    if st.checkbox('Line chart'):
+        st.line_chart(display_data)
 
-        if st.checkbox('Line chart'):
-            st.line_chart(display_data)
+    if st.checkbox('Bar chart'):
+        st.bar_chart(display_data)
 
-        if st.checkbox('Bar chart'):
-            st.bar_chart(display_data)
+    if len(display_data.columns) >= 2:
+        if st.checkbox('Variable relationship (scatter matrix)'):
+            dim = len(selected_cols) * 2
+            fig = plt.figure(figsize=(dim, dim))
+            ax = fig.gca()
+            scatter_matrix(display_data, ax=ax)
+            st.write(fig)
 
-        if len(display_data.columns) >= 2:
-            if st.checkbox('Variable relationship (scatter matrix)'):
-                dim = len(selected_cols) * 2
-                fig = plt.figure(figsize=(dim, dim))
-                ax = fig.gca()
-                scatter_matrix(display_data, ax=ax)
-                st.write(fig)
+    if ('lat' in selected_cols) and ('lon' in selected_cols):
+        if st.checkbox('Show on map'):
+            st.map(display_data)
 
-        if ('lat' in selected_cols) and ('lon' in selected_cols):
-            if st.checkbox('Show on map'):
-                st.map(display_data)
-    else:
-        st.markdown('## Select at least one column')
+    if st.checkbox('Regression'):
+        x = st.radio('X', selected_cols)
+        y = st.radio('Y', selected_cols)
+
+        ir = IsoRegressor(display_data)
+        fig = ir.get_graph(x, y)
+        st.write(fig)
+
+        input = st.text_input(f'Enter a value for {x} to predict {y}')
+
+        if st.button('Predict'):
+            pred = ir.make_prediction(float(input))
+            pred = pred[0] # need to get the first value since the prediction is returned as a list
+            st.write(f'Predicted {y} = {round(pred, 3)}')
 else:
-    st.markdown('## Click Explore to start')
+    st.markdown('## Select at least one column')
---
+++ iso_regression.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.collections import LineCollection
+from sklearn.isotonic import IsotonicRegression
+
+class IsoRegressor:
+
+    dataset = []
+    ir = None
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.ir = IsotonicRegression()
+
+    def select_columns(self, x, y):
+        x = np.array(self.dataset[x])
+        y = np.array(self.dataset[y])
+
+        # can't just sort data because it messes up the relationship between x and y
+        x = np.sort(x)
+        y = np.sort(y)
+
+        return x, y
+
+    def get_graph(self, col1, col2):
+        x, y = self.select_columns(col1, col2)
+        transformed = self.ir.fit_transform(x, y)
+
+        n = len(x)
+        segments = [[[i, y[i]], [i, transformed[i]]] for i in range(n)]
+        lc = LineCollection(segments, zorder=0)
+        lc.set_array(np.ones(len(y)))
+        lc.set_linewidth(np.full(n, 0.5))
+
+        fig = plt.figure(figsize=(15,8))
+        plt.plot(x, y, 'r.', markersize=12)
+        plt.plot(x, transformed, 'b.-', markersize=12)
+        plt.gca().add_collection(lc)
+        plt.legend(('Data', 'Isotonic Fit'), loc='lower right')
+        plt.title('Isotonic regression')
+        
+        return fig
+
+    def make_prediction(self, input):
+        feature = [input]
+        pred = self.ir.predict(feature)
+
+        return pred
